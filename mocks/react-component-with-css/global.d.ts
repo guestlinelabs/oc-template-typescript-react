b87c0bb6060ffdb7dc6220f0a0ac9379d5e2bf97
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2019-12-18 19:24:07 +0100

    fix react

--- mocks/react-component-with-css/global.d.ts
+++ mocks/react-component-with-css/global.d.ts
@@ -1,4 +1,11 @@
-declare module "*.css" {
+declare module '*.css' {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+declare var oc: {
+  events: {
+    on: (eventName: string, fn: (...data: any[]) => void) => void;
+    fire: (eventName: string, data?: any) => void;
+  };
+};
--- packages/oc-template-typescript-react-compiler/scaffold/src/global.d.ts
+++ packages/oc-template-typescript-react-compiler/scaffold/src/global.d.ts
@@ -2,3 +2,10 @@ declare module "*.css" {
   const classes: { readonly [key: string]: string };
   export default classes;
 }
+
+declare var oc: {
+  events: {
+    on: (eventName: string, fn: (...data: any[]) => void) => void;
+    fire: (eventName: string, data?: any) => void;
+  };
+};
