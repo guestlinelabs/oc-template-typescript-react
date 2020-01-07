f6734093fd588eae537fc541cb058f78ff378c4c
Ricardo Devis Agullo <ricardo.devisagullo@guestline.com>
2020-01-07 16:40:44 +0100

    update scaffold with tests

---
+++ packages/oc-template-typescript-react-compiler/scaffold/src/src/app.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { mount } from "enzyme";
+
+import App from "./app";
+
+const getData = jest.fn();
+
+describe("App - Page", () => {
+  beforeEach(() => {
+    window.oc = { events: { on: jest.fn(), fire: jest.fn() } };
+  });
+
+  it("Gets more data when clicking the button", () => {
+    const renderedComponent = mount(<App getData={getData} />);
+
+    renderedComponent.find("button").simulate("click");
+
+    expect(getData).toHaveBeenCalled();
+  });
+});
