9adc478d700c4322a3632cfb80ae5691cd967d12
Ricardo Devis Agullo <mkredz@gmail.com>
2020-11-16 12:08:58 +0100

    Fix issue with css modules not working Because oc-app.d.ts had declare modules and exports it didnt work as a global export. So we moved the css declarations into oc-template-typescript-react package and the Server exports in the compiler package. That should solve the problem.
    Add rest

--- packages/oc-template-typescript-react-compiler/lib/oc-app.d.ts
+++ packages/oc-template-typescript-react-compiler/lib/oc-app.d.ts
@@ -1,22 +1,3 @@
-/// <reference types="node" />
-/// <reference types="react" />
-/// <reference types="react-dom" />
-
-declare module '*.css' {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
-declare module '*.scss' {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
-declare module '*.sass' {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
 declare global {
   interface Window {
     oc: {
---
+++ packages/oc-template-typescript-react/lib/oc-app.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="node" />
+/// <reference types="react" />
+/// <reference types="react-dom" />
+
+declare module '*.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+declare module '*.sass' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
