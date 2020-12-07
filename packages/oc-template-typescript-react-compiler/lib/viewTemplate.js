eb5e3628fdb4ae4fce51f88de59b56baf295aa11
Piotr Bazydło <piotr.bazydlo@guestline.com>
2020-12-07 16:08:51 +0100

    Allow rendering multiple components on a page

--- packages/oc-template-typescript-react-compiler/lib/viewTemplate.js
+++ packages/oc-template-typescript-react-compiler/lib/viewTemplate.js
@@ -8,7 +8,11 @@ const viewTemplate = ({
   var modelHTML =  model.__html ? model.__html : '';
   var staticPath = model.reactComponent.props._staticPath;
   var props = JSON.stringify(model.reactComponent.props);
-  return '<div id="${reactRoot}" class="${reactRoot}">' + modelHTML + '</div>' +
+  window.oc = window.oc || {};
+  window.oc.__typescriptReactTemplate = window.oc.__typescriptReactTemplate || { count: 0 };
+  var count = window.oc.__typescriptReactTemplate.count;
+  window.oc.__typescriptReactTemplate.count++;
+  return '<div id="${reactRoot}-' + count + '" class="${reactRoot}">' + modelHTML + '</div>' +
     '${css ? "<style>" + css + "</style>" : ""}' +
     '<script>' +
     'window.oc = window.oc || {};' +
@@ -20,7 +24,7 @@ const viewTemplate = ({
           '["oc", "reactComponents", "${bundleHash}"],' + 
           '"' + staticPath + '${bundleName}.js",' +
           'function(ReactComponent){' +
-            'var targetNode = document.getElementById("${reactRoot}");' +
+            'var targetNode = document.getElementById("${reactRoot}-' + count + '");' +
             'targetNode.setAttribute("id","");' +
             'ReactDOM.render(React.createElement(ReactComponent,' +  props + '),targetNode);' +
           '}' +
