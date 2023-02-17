2fddb46ddb1a8966804ac358256c163430388b9b
Ricardo Devis Agullo <mkredz@gmail.com>
2023-02-17 20:30:21 +0100

    move to react18

--- packages/oc-template-typescript-react-compiler/scaffold/src/src/app.test.tsx
+++ packages/oc-template-typescript-react-compiler/scaffold/src/src/app.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 
 import App from './app';
 
@@ -11,12 +11,11 @@ describe('App - Page', () => {
   });
 
   it('Gets more data when clicking the button', () => {
-    const renderedComponent = mount(
+    render(
       <App firstName="firstName" lastName="lastName" userId={0} getData={getData} />
     );
 
-    renderedComponent.find('button').simulate('click');
-
-    expect(getData).toHaveBeenCalled();
+    const hobbiesElement = screen.getByText(/Hobbies/i);
+    expect(hobbiesElement).toBeInTheDocument();
   });
 });
