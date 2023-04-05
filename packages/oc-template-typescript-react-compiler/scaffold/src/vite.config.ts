508421d5d69841c83d0bfd62b38a05ebf469773b
Ricardo Devis Agullo <mkredz@gmail.com>
2023-04-05 09:20:57 +0200

    plugins sections

--- packages/oc-template-typescript-react-compiler/scaffold/src/vite.config.ts
+++ packages/oc-template-typescript-react-compiler/scaffold/src/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Add your own plugins here for thinkgs like eslint or typecheckers
+  plugins: [],
   // @ts-ignore Missing test property
   test: {
     environment: 'jsdom'
